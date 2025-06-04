6650eba9c30487c484545ef63a39ba8f480c7172
ElviraKondratova <elvirochka_sh@mail.ru>
2025-06-04 12:51:27 +0300

    1dz

---
+++ main.py
@@ -0,0 +1,41 @@
+from flask import Flask
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def show_current_time():
+    # Получаем текущие дату и время
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    # Формируем HTML-страницу с данными
+    return f'''
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Текущее время</title>
+        <meta charset="utf-8">
+        <style>
+            body {{ 
+                font-family: Arial, sans-serif; 
+                text-align: center;
+                margin-top: 50px;
+            }}
+            .time {{
+                font-size: 2em;
+                color: #333;
+                padding: 20px;
+                background-color: #f0f0f0;
+                border-radius: 10px;
+                display: inline-block;
+            }}
+        </style>
+    </head>
+    <body>
+        <h1>Текущие дата и время</h1>
+        <div class="time">{current_time}</div>
+    </body>
+    </html>
+    '''
+
+if __name__ == '__main__':
+    app.run(debug=True)
